2fc6290ef8827359c6ff87a3a09baeb054fc4463
BingCheng Chiang <b09901044@ntu.edu.tw>
2023-11-22 15:09:44 +0800

    clear

--- DCLAB3_pro/src/Top.sv
+++ DCLAB3_pro/src/Top.sv
@@ -152,12 +152,10 @@ AudDSP dsp0(
 // receive data address from DSP and fetch data to sent to WM8731 with I2S protocal
 AudPlayer player0(
 	.i_rst_n(i_rst_n),
-	.i_bclk(i_AUD_BCLK),
-	.i_daclrck(i_AUD_DACLRCK),
+	.i_clk(i_AUD_BCLK),
+	.i_lrc(i_AUD_DACLRCK),
 	.i_en(play_en), // enable AudPlayer only when playing audio, work with AudDSP
 	.i_dac_data(dac_data), //dac_data
-	.i_volume_l(i_volume_l),
-	.i_volume_r(i_volume_r),
 	.o_aud_dacdat(o_AUD_DACDAT),
 	.o_state(o_state_PLAY)
 );
